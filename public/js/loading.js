2eb8dc33c7f3427026a35ec6360e1aa5d82bc04d
Neville Jeremy <124435554+nepile@users.noreply.github.com>
2024-01-09 03:39:55 +0700

    feat: create loading animation

---
+++ public/js/loading.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("form").submit(function () {
+        $("#loading-popup").show();
+        $("#loading-overlay").show();
+    });
+});
--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -8,16 +8,26 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="icon" href="{{ asset('img/logo.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/notiflix@3.2.6/dist/notiflix-3.2.6.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/loading.css') }}">
     @stack('auth-style')
     @stack('core-style')
 </head>
 <body>
+
+    <div id="loading-overlay"></div>
+  
+    <div id="loading-popup">
+      <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+      <p class="text-center" style="font-size: 12px">Loading...</p>
+      <img src="{{ asset('img/logo.png') }}" width="180" alt="">
+    </div>
 
     {{-- main --}}
     @yield('content')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/notiflix@3.2.6/dist/notiflix-aio-3.2.6.min.js"></script>
+    <script src="{{ asset('js/loading.js') }}"></script>
     @include('template.notiflix')
     @stack('core-script')
 </body>
